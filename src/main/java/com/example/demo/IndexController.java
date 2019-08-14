5146e9e2140736d0ade0cbd6b4f5fe4382b3b49d
zhoulei <zhoulei@xylink.com>
2019-08-14 15:55:40 +0800

    fix

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.model.ResponseData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -8,9 +9,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSONObject;
 
+import java.util.*;
+
 
 @Controller
 public class IndexController {
+    public final  Map<Integer,String> m=new HashMap<>();
+    public final List<ResponseData> m1=new ArrayList<>();
     @RequestMapping(value = {"/jsp"})
     public String indexJsp(Model model) {
     	  model.addAttribute("name", "张三");
@@ -28,5 +33,22 @@ public class IndexController {
     	JSONObject j = new JSONObject();
     	j.put("name", StringUtils.isEmpty(name)? "John Doe " :  name);
         return j;
+    }
+
+    @RequestMapping("/string")
+    @ResponseBody
+    public JSONObject string() {
+        for (int i = 0; i < 10000; i++) {
+            m.put(i, UUID.randomUUID().toString());
+        }
+        return new JSONObject();
+    }
+    @RequestMapping("/object")
+    @ResponseBody
+    public JSONObject object() {
+        for (int i = 0; i < 10000; i++) {
+            m1.add(new ResponseData(0,UUID.randomUUID().toString(),null));
+        }
+        return new JSONObject();
     }
 }
